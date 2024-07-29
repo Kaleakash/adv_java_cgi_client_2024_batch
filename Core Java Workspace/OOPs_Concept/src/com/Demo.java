c085e62609c8bc96a1c86e89e91cf4b3205f5148
akash <akash300383@gmail.com>
2024-07-29 11:14:19 +0530

    initial commit

---
+++ Core Java Workspace/OOPs_Concept/src/com/Demo.java
@@ -0,0 +1,12 @@
+package com;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Java");
+		int a=10;
+		int b=20;
+		int sum = a+b;
+		System.out.println("sum is "+sum);
+	}
+}
