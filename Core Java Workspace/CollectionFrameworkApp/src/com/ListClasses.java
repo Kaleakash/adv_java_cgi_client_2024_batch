45014202e6a2ad924ce0ab0722cb700eaff466be
akash <akash300383@gmail.com>
2024-08-01 10:52:47 +0530

    done

--- Core Java Workspace/CollectionFrameworkApp/src/com/ListClasses.java
+++ Core Java Workspace/CollectionFrameworkApp/src/com/ListClasses.java
@@ -3,6 +3,7 @@ package com;
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Stack;
 import java.util.Vector;
 
 public class ListClasses {
@@ -42,14 +43,29 @@ public class ListClasses {
 //		ll1.remove(3);	// remove using index 
 //		System.out.println(ll1);
 		
+		
 		// Vector class 
 		
 		//List ll1 = new Vector();
-		Vector ll1 = new Vector();
-		ll1.add(10);
-		ll1.addElement(20);
-		System.out.println(ll1);
+//		Vector ll1 = new Vector();
+//		ll1.add(10);
+//		ll1.addElement(20);
+//		System.out.println(ll1);
 		
+		// Stack 
+		Stack ss = new Stack();
+		ss.push(10);
+		ss.push(20);
+		ss.push(30);
+		ss.push(40);
+		System.out.println(ss);
+		System.out.println("remove top most elemenet "+ss.pop());
+		System.out.println(ss);
+		System.out.println("display top most elemenet "+ss.peek());
+		System.out.println(ss);
+		System.out.println("size "+ss.size());
+		System.out.println("search "+ss.search(10));
+		System.out.println("search "+ss.search(100));
 	}
 
 }
