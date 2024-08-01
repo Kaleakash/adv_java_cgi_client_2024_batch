0dbd282d79a950d54bea0f6518225fa2f4c7c341
akash <akash300383@gmail.com>
2024-08-01 11:24:31 +0530

    done

---
+++ Core Java Workspace/CollectionFrameworkApp/src/com/MapClasses.java
@@ -0,0 +1,25 @@
+package com;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MapClasses {
+
+	public static void main(String[] args) {
+	// HashMap 
+	Map mm = new HashMap();
+	mm.put(2, "Ravi");
+	mm.put(1, "Ramesh");
+	mm.put("a", "b");
+	mm.put(3, "Ajay");
+	mm.put(4, "Balaji");
+	mm.put(4, "Yogesh");
+	System.out.println(mm);
+	System.out.println("key present "+mm.containsKey(4));
+	System.out.println("key present "+mm.containsKey(40));
+	System.out.println("value present "+mm.containsValue("Ravi"));
+	System.out.println("value present "+mm.containsValue("Raju"));
+	System.out.println("get value "+mm.get(1));
+	}
+
+}
