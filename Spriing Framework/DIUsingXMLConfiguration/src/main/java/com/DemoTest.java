5edc5845ed2db886b55e6cad76c2fc831b904dea
akash <akash300383@gmail.com>
2024-08-07 10:20:14 +0530

    done

--- Spriing Framework/DIUsingXMLConfiguration/src/main/java/com/DemoTest.java
+++ Spriing Framework/DIUsingXMLConfiguration/src/main/java/com/DemoTest.java
@@ -13,18 +13,28 @@ public class DemoTest {
 		Resource res = new ClassPathResource("beans.xml");  // load the file get the resource reference
 		BeanFactory obj = new XmlBeanFactory(res);	// get the BeanFactory reference. 
 		
+//		Employee emp1 = (Employee)obj.getBean("emp1");
+//		emp1.display();
+//		
+//		Employee emp3 = (Employee)obj.getBean("emp1");
+//		emp3.display();
+//		
+//		Employee emp2 = (Employee)obj.getBean("emp2");
+//		emp2.display();
+//		
+//		Employee emp4 = (Employee)obj.getBean("emp2");
+//		emp4.display();
+		
 		Employee emp1 = (Employee)obj.getBean("emp1");
-		emp1.display();
-		
-		Employee emp3 = (Employee)obj.getBean("emp1");
-		emp3.display();
-		
+		System.out.println(emp1);
 		Employee emp2 = (Employee)obj.getBean("emp2");
-		emp2.display();
+		System.out.println(emp2);
 		
-		Employee emp4 = (Employee)obj.getBean("emp2");
-		emp4.display();
+		Employee emp3 = (Employee)obj.getBean("emp3");
+		System.out.println(emp3);
 		
+		Employee emp4 = (Employee)obj.getBean("emp4");
+		System.out.println(emp4);
 	}
 
 }
