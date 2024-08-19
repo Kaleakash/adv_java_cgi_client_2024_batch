b27f8ac75324264e5879e4866abce4f5f7e8ac9c
akash <akash300383@gmail.com>
2024-08-19 12:14:37 +0530

    done

--- spring boot workspace/FirstSpringBootApp/src/main/java/com/main/DemoTest.java
+++ spring boot workspace/FirstSpringBootApp/src/main/java/com/main/DemoTest.java
@@ -1,9 +1,9 @@
-package com;
+package com.main;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com")
 public class DemoTest {
 
 	public static void main(String[] args) {
