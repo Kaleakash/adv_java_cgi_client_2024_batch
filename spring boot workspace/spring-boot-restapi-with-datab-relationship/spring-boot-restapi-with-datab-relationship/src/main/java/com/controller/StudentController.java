4eb0a2a06578d40a4d69e25b061aab5af7079981
akash <akash300383@gmail.com>
2024-08-27 10:53:05 +0530

    done

--- spring boot workspace/spring-boot-restapi-with-datab-relationship/spring-boot-restapi-with-datab-relationship/src/main/java/com/controller/StudentController.java
+++ spring boot workspace/spring-boot-restapi-with-datab-relationship/spring-boot-restapi-with-datab-relationship/src/main/java/com/controller/StudentController.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,9 +21,30 @@ public class StudentController {
 	
 	// http://localhost:8080/students
 	
-	@RequestMapping(value = "students",method = RequestMethod.GET,
+	@RequestMapping(value = "students",
+			method = RequestMethod.GET,
 			produces = MediaType.APPLICATION_JSON_VALUE)
 	public List<Student> findAll() {
 		return studentService.findAll();
+	}
+	
+	@RequestMapping(value = "storestudent",
+			method = RequestMethod.POST,
+			consumes = MediaType.APPLICATION_JSON_VALUE)
+	public String storeStudent(@RequestBody Student student) {
+		return studentService.storeStudent(student);
+	}
+	
+	@RequestMapping(value = "updatestudent",
+			method = RequestMethod.PUT,
+			consumes = MediaType.APPLICATION_JSON_VALUE)
+	public String updateStudent(@RequestBody Student student) {
+		return studentService.updateStudentAge(student);
+	}
+	
+	@RequestMapping(value = "deletestudent/{sid}",
+			method = RequestMethod.DELETE)
+	public String deleteStudent(@PathVariable("sid") int sid) {
+		return studentService.deleteStudent(sid);
 	}
 }
--- spring boot workspace/spring-boot-restapi-with-datab-relationship/spring-boot-restapi-with-datab-relationship/src/main/java/com/controller/TrainerController.java
+++ spring boot workspace/spring-boot-restapi-with-datab-relationship/spring-boot-restapi-with-datab-relationship/src/main/java/com/controller/TrainerController.java
@@ -5,10 +5,13 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.MediaTypeEditor;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.entity.Student;
 import com.entity.Trainer;
 import com.service.TrainerService;
 
@@ -23,5 +26,25 @@ public class TrainerController {
 			method = RequestMethod.GET)
 	public List<Trainer> findAll() {
 		return trainerService.findAll();
+	}
+	
+	@RequestMapping(value = "storetrainer",
+			method = RequestMethod.POST,
+			consumes = MediaType.APPLICATION_JSON_VALUE)
+	public String storeTrainer(@RequestBody Trainer trainer) {
+		return trainerService.storeTrainer(trainer);
+	}
+	
+	@RequestMapping(value = "updatetrainer",
+			method = RequestMethod.PUT,
+			consumes = MediaType.APPLICATION_JSON_VALUE)
+	public String updateTrainer(@RequestBody Trainer trainer) {
+		return trainerService.updateTrainer(trainer);
+	}
+	
+	@RequestMapping(value = "deletetrainer/{tid}",
+			method = RequestMethod.DELETE)
+	public String deleteTrainer(@PathVariable("tid") int tid) {
+		return trainerService.deleteTraienr(tid);
 	}
 }
