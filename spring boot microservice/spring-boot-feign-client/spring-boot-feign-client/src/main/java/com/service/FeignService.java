5017b96752ed29375d67ed9352a8d30b6fcf1f78
Akash <akash300383@gmail.com>
2024-09-03 10:16:33 +0530

    done

---
+++ spring boot microservice/spring-boot-feign-client/spring-boot-feign-client/src/main/java/com/service/FeignService.java
@@ -0,0 +1,22 @@
+package com.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class FeignService {
+
+	@Autowired
+	AccountFeignService accountFiegnService;
+	
+	@Autowired
+	GpayFeignService gpayFeignService;
+	
+	public String findAccountDetails(int acno) {
+		return accountFiegnService.findBalance(acno);
+	}
+	
+	public String findBalance(String emailid) {
+		return gpayFeignService.findBalace(emailid);
+	}
+}
