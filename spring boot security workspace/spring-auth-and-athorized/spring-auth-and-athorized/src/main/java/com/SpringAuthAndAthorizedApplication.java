f9793d49d5cd09425c8986349f3ac8b54554d3a7
Akash <akash300383@gmail.com>
2024-09-11 12:12:07 +0530

    done

---
+++ spring boot security workspace/spring-auth-and-athorized/spring-auth-and-athorized/src/main/java/com/SpringAuthAndAthorizedApplication.java
@@ -0,0 +1,13 @@
+package com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAuthAndAthorizedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAuthAndAthorizedApplication.class, args);
+	}
+
+}
